9b678aac5f64d3a268a057abff656e2c39bed6d7
Andi <andi.tan@hellofresh.com.au>
2022-07-28 15:19:54 +1000

    keybinding for nvim-tree

--- lua/general/keybindings.lua
+++ lua/general/keybindings.lua
@@ -4,4 +4,8 @@ local function nkeymap(key, map)
     keymap('n', key, map, opts)
 end
 
-nkeymap('gd',':lua vim.lsp.buf.definition()<cr>')
+--nkeymap('gd',':lua vim.lsp.buf.definition()<cr>')
+keymap("n", "gd", ":lua vim.lsp.buf.definition()<cr>", {noremap = true, silent = true})
+
+
+keymap("n", "<C-f>", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
