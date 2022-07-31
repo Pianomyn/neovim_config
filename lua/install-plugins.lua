24eff1b1a99d6b0fab437d83014eed89705a3a97
Pianomyn <anditan2014@gmail.com>
2022-07-31 13:56:48 +1000

    formatted install-plugins.lua

--- lua/install-plugins.lua
+++ lua/install-plugins.lua
@@ -1,79 +1,80 @@
 local install_path = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-  vim.cmd [[packadd packer.nvim]]
+    packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    vim.cmd [[packadd packer.nvim]]
 end
 
-local packer = require("packer")
-packer.init({
-compile_on_sync = false
+require("packer").init({
+    compile_on_sync = false
 })
-return packer.startup(function(use)
 
-	-- Editor
-	use ({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-            "kyazdani42/nvim-web-devicons",
-		},
-		config = function()
-			require("plugins/nvim-tree")
-		end,
-		tag = "nightly" -- optional, updated every week. (see issue #1193)
-	})
-	use ({
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("plugins/lualine")
-		end,
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	})
+return packer.startup(
+    function(use)
 
-	-- Language
-	use ({
-        "neovim/nvim-lspconfig",
-        requires = {
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp",
-            "williamboman/nvim-lsp-installer",
-            "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
-        },
-        config = function()
-            require("plugins/nvim-lspconfig")
+        -- Editor
+        use ({
+            "kyazdani42/nvim-tree.lua",
+            requires = {
+                "kyazdani42/nvim-web-devicons",
+            },
+            config = function()
+                require("plugins/nvim-tree")
+            end,
+            tag = "nightly" -- optional, updated every week. (see issue #1193)
+        })
+        use ({
+            "nvim-lualine/lualine.nvim",
+            config = function()
+                require("plugins/lualine")
+            end,
+            requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        })
+
+        -- Language
+        use ({
+            "neovim/nvim-lspconfig",
+            requires = {
+                "hrsh7th/nvim-cmp",
+                "hrsh7th/cmp-nvim-lsp",
+                "williamboman/nvim-lsp-installer",
+                "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
+            },
+            config = function()
+                require("plugins/nvim-lspconfig")
+            end
+        })
+        use ({
+            "nvim-treesitter/nvim-treesitter", 
+            requires = {
+                "p00f/nvim-ts-rainbow",
+            },
+            config = function()
+                require("plugins/nvim-treesitter")
+            end
+        })
+        use({
+            "junegunn/fzf.vim",
+            requires = {
+                "junegunn/fzf"
+            }
+        })
+
+        -- Git
+        use "airblade/vim-gitgutter"
+        use "tpope/vim-fugitive"
+        use ({
+            "APZelos/blamer.nvim",
+            config = function()
+                require("plugins/blamer")
+            end
+        })
+
+        -- Colors, Icons
+        use "rafi/awesome-vim-colorschemes"
+        use "tomasiser/vim-code-dark"
+
+        if packer_bootstrap then
+            require("packer").sync()
         end
-    })
-	use ({
-        "nvim-treesitter/nvim-treesitter", 
-        requires = {
-            "p00f/nvim-ts-rainbow",
-        },
-        config = function()
-            require("plugins/nvim-treesitter")
-        end
-    })
-    use({
-        "junegunn/fzf.vim",
-        requires = {
-            "junegunn/fzf"
-        }
-    })
-
-	-- Git
-	use "airblade/vim-gitgutter"
-	use "tpope/vim-fugitive"
-	use ({
-        "APZelos/blamer.nvim",
-        config = function()
-            require("plugins/blamer")
-        end
-    })
-
-	-- Colors, Icons
-	use "rafi/awesome-vim-colorschemes"
-	use "tomasiser/vim-code-dark"
-
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+    end
+)
