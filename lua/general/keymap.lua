1979dcea2567676ee47506185d2e66ccf9fb25b6
Andi <andi.tan@hellofresh.com.au>
2022-08-03 00:34:52 +1000

    keymaps:

--- lua/general/keymap.lua
+++ lua/general/keymap.lua
@@ -2,11 +2,13 @@ local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
 keymap("n", "gd", ":lua vim.lsp.buf.definition()<cr>", opts)
+keymap("n", "gD", ":lua vim.lsp.buf.declaration()<cr>", opts)
 
 keymap("n", "<C-f>", ":NvimTreeToggle<CR>", opts)
 keymap("n", "<C-p>", ":Files<CR>", opts)
 keymap("n", "<C-g>", ":Rg<CR>", opts)
 
-keymap("n", "<Leader>d", ":BlamerToggle<CR>", opts)
+keymap("n", "<Leader>d", ":ToggleDiag<CR>", opts)
+keymap("n", "gb", ":BlamerToggle<CR>", opts)
 
 keymap("n", "<Leader>`", ":!", {noremap = true})
