3ffd5f3bc84f01f2b0f2bfbd043be34462289aef
Andi <andi.tan@hellofresh.com.au>
2022-08-03 11:03:57 +1000

    autoindent

--- lua/general/editor.lua
+++ lua/general/editor.lua
@@ -12,6 +12,7 @@ vim.cmd("set paste")
 vim.cmd("set encoding=utf-8")
 
 vim.cmd("set expandtab")
+vim.cmd("set autoindent")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set smarttab")
