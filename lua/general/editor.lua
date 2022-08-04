3e7e4df7c9ebaa11419df24c08220a54d6c37605
Andi <andi.tan@hellofresh.com.au>
2022-08-04 10:49:42 +1000

    Removed useless line

--- lua/general/editor.lua
+++ lua/general/editor.lua
@@ -10,7 +10,6 @@ vim.wo.colorcolumn = "120"
 vim.cmd("silent! colorscheme codedark")
 vim.cmd("set paste")
 vim.cmd("set encoding=utf-8")
-vim.cmd("DoMatchParen")
 
 vim.cmd("set expandtab")
 vim.cmd("set autoindent")
