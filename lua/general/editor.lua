06bbad820f53e8f1042f924d482661575581b6e4
Andi <andi.tan@hellofresh.com.au>
2022-08-04 16:21:05 +1000

    highlight instances of the selected word with *

--- lua/general/editor.lua
+++ lua/general/editor.lua
@@ -10,6 +10,7 @@ vim.wo.colorcolumn = "120"
 vim.cmd("silent! colorscheme codedark")
 vim.cmd("set paste")
 vim.cmd("set encoding=utf-8")
+vim.cmd("set hlsearch")
 
 vim.cmd("set expandtab")
 vim.cmd("set autoindent")
