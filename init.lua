45bf7f94d687c1bd3974458ae00b8cc675b9a08c
Pianomyn <anditan2014@gmail.com>
2022-08-11 21:51:45 +1000

    asdf

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-require("install-plugins")
-require("general")
-
-require("plugins/cmp")
---require("plugins/lsp")
-
-require("plugins/nvim-tree")
-require("plugins/lualine")
-require("plugins/nvim-treesitter")
-require("plugins/diffview")
-require("plugins/blamer")
+require('plugins')
+require('keybindings')
+require('options')
