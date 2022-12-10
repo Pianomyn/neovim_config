fa82cea44eee639cf804e960adb779381887362c
Andi <andi.tan@hellofresh.com.au>
2022-12-10 16:10:08 +1100

    gitsigns plugin

--- lua/install-plugins.lua
+++ lua/install-plugins.lua
@@ -93,7 +93,12 @@ return packer.startup(function(use)
     })
     use "tpope/vim-fugitive"
     use "APZelos/blamer.nvim"
-    use "lewis6991/gitsigns.nvim"
+    use {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require("gitsigns").setup()
+      end
+    }
 
     -- Colors, Themes 
     use "rafi/awesome-vim-colorschemes"
