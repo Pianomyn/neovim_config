f276aafc5c8e8ed14596f03e68723f2c43f48788
Andi <andi.tan@hellofresh.com.au>
2023-01-13 14:15:39 +1100

    formatted with stylua

--- lua/install-plugins.lua
+++ lua/install-plugins.lua
@@ -1,7 +1,8 @@
-local install_path = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-    vim.cmd("packadd packer.nvim")
+	packer_bootstrap =
+		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd("packadd packer.nvim")
 end
 
 vim.cmd([[
@@ -11,101 +12,99 @@ augroup packer_user_config
 augroup end
 ]])
 
-
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
 packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({border = "rounded"})
-        end
-    },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
 return packer.startup(function(use)
+	-- Packer
+	use("wbthomason/packer.nvim")
 
-    -- Packer
-    use ("wbthomason/packer.nvim")
+	-- Package Installer
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"jayp0521/mason-null-ls.nvim",
+	})
 
-    -- Package Installer
-    use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "jayp0521/mason-null-ls.nvim",
-    }
+	-- Editor
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+	})
+	use("nvim-lualine/lualine.nvim")
+	use({
+		"junegunn/fzf.vim",
+		requires = {
+			"junegunn/fzf",
+		},
+	})
+	use("lukas-reineke/indent-blankline.nvim")
+	use("RRethy/vim-illuminate")
 
-    -- Editor
-    use ({ 
-        "kyazdani42/nvim-tree.lua",
-        requires = {
-            "kyazdani42/nvim-web-devicons"
-        },
-    })
-    use "nvim-lualine/lualine.nvim"
-    use ({
-        "junegunn/fzf.vim",
-        requires = {
-            "junegunn/fzf",
-        },
-    })
-    use "lukas-reineke/indent-blankline.nvim"
-    use "RRethy/vim-illuminate"
+	-- Language
+	use("neovim/nvim-lspconfig")
+	use("WhoIsSethDaniel/toggle-lsp-diagnostics.nvim")
+	use("ray-x/lsp_signature.nvim")
 
-    -- Language
-    use "neovim/nvim-lspconfig"
-    use "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim"
-    use "ray-x/lsp_signature.nvim"
+	-- Formatting and Linting
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+	})
 
-    -- Formatting and Linting
-    use {
-      "jose-elias-alvarez/null-ls.nvim",
-    }
+	-- Autocomplete
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lua",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	})
 
-    -- Autocomplete
-    use ({
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lua",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-        },
-    })
+	-- Syntax
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		requires = {
+			"p00f/nvim-ts-rainbow",
+		},
+	})
 
-    -- Syntax
-    use ({
-        "nvim-treesitter/nvim-treesitter",
-        requires = {
-            "p00f/nvim-ts-rainbow",
-        },
-    })
+	-- Git
+	use({
+		"sindrets/diffview.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
+	use("tpope/vim-fugitive")
+	use("APZelos/blamer.nvim")
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
 
-    -- Git
-    use ({
-        "sindrets/diffview.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-    })
-    use "tpope/vim-fugitive"
-    use "APZelos/blamer.nvim"
-    use {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("gitsigns").setup()
-      end
-    }
+	-- Colors, Themes
+	use("rafi/awesome-vim-colorschemes")
+	use("tomasiser/vim-code-dark")
+	use("shaunsingh/nord.nvim")
 
-    -- Colors, Themes 
-    use "rafi/awesome-vim-colorschemes"
-    use "tomasiser/vim-code-dark"
-    use "shaunsingh/nord.nvim"
-
-    if packer_bootstrap then
-        packer.sync()
-    end
+	if packer_bootstrap then
+		packer.sync()
+	end
 end)
