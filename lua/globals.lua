6048b6be9c124b20db180752f1a5f5c0622d0cf9
Pianomyn <anditan2014@gmail.com>
2023-01-14 14:54:13 +1100

    moved required grammars to a global var

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,3 +1,5 @@
-REQUIRED_LANGUAGE_SERVERS = {"pyright", "tsserver", "html", "cssls", "sumneko_lua", "marksman"}
+REQUIRED_LANGUAGE_SERVERS = { "pyright", "tsserver", "html", "cssls", "sumneko_lua", "marksman" }
 
-REQUIRED_LINTERS_FORMATTERS = {"isort", "black", "prettier", "stylua"}
+REQUIRED_LINTERS_FORMATTERS = { "isort", "black", "prettier", "stylua" }
+
+REQUIRED_GRAMMARS = { "python", "javascript", "typescript", "html", "css", "lua", "sql" }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,16 @@
-local grammar_list =
-	{ "python", "javascript", "typescript" },
-	
 require("nvim-treesitter.configs").setup({
-		ensure_installed = grammar_list,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		rainbow = {
-			enable = true,
-			extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-			max_file_lines = nil, -- Do not enable for files with more than n lines, int
-			-- colors = {}, -- table of hex strings
-			-- termcolors = {} -- table of colour name strings
-		},
-	})
+	ensure_installed = REQUIRED_GRAMMARS,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+})
