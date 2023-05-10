29580fb91ba3c240f6f6a1112a89f542fa364544
Andi <andi.tan@hellofresh.com.au>
2023-05-10 11:23:19 +1000

    using onedark statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,10 @@
-local base16 = require("lualine.themes.base16")
-local codedark = require("lualine.themes.codedark")
+local base16 = require("lualine.themes.base16") -- command and normal same color
+local codedark = require("lualine.themes.codedark") -- command and normal same color
 local onedark = require("lualine.themes.onedark")
 local nord = require("lualine.themes.nord")
 
 require("lualine").setup({
 	options = {
-		theme = base16,
+		theme = onedark,
 	},
 })
