7e87de40bc7dce46bd8a14511310577a905ffd4b
Pianomyn <anditan2014@gmail.com>
2023-06-14 12:22:35 +1000

    show files search in fullscreen view, preview at top

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,6 +6,14 @@ command! -bang -nargs=* Rg
   \   fzf#vim#with_preview('up', 'ctrl-/'), 1)
 ]])
 
+-- Show Files search in fullscreen with preview window at the top
+vim.cmd([[
+command! -bang -nargs=* Files
+  \ call fzf#vim#grep(
+  \   "rg --column --line-number --no-heading --color=always --smart-case -- ".shellescape(<q-args>), 1,
+  \   fzf#vim#with_preview('up', 'ctrl-/'), 1)
+]])
+
 -- Make Ripgrep search with custom glob (ex. *.md for md files)
 vim.cmd([[
 command! -bang -nargs=* RgFileExt call fzf#vim#grep("rg --column --line-number --hidden --smart-case --no-heading --color=always --glob '!**/{.git,node_modules,.idea,.vscode,.history}/**' -g '" . <q-args> . "' ''", 1, <bang>0)
