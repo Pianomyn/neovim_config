17523373ce5e19d3943f980f865f2a4919a6b92a
Andi <andi.tan@hellofresh.com.au>
2023-08-24 11:18:13 +1000

    new keymap for closing tab

--- lua/general/keymap.lua
+++ lua/general/keymap.lua
@@ -15,7 +15,7 @@ keymap("n", "<leader>rbf", ":Refactor extract_block_to_file<CR>", opts)
 -- Tabs
 keymap("n", "<S-h>", ":BufferPrevious<CR>", opts)
 keymap("n", "<S-l>", ":BufferNext<CR>", opts)
-keymap("n", "<Leader>w", ":BufferClose<CR>", opts)
+keymap("n", "<Leader>x", ":BufferClose<CR>", opts)
 keymap("n", "<Leader>q", ":BufferClose!<CR>", opts)
 keymap("n", "<C-t>", ":tabnew<CR>", opts)
 keymap("n", "ts", ":tab split<CR>", opts) -- Duplicate current tab
