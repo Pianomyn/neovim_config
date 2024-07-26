046e28566adc4969679e674111876ab0e34a5b49
Pianomyn <anditan@skiff.com>
2024-07-26 23:20:09 +1000

    refactor: moves all plugin related code to a module

--- init.lua
+++ init.lua
@@ -1,21 +1,9 @@
 -- Default
 require("globals")
-require("install-plugins")
+require("plugins")
 
 require("core")
 require("appearance")
 
 -- Plugins
-require("plugins/mason")
-require("plugins/fzf")
-require("plugins/cmp")
-require("plugins/lsp")
-require("plugins/nvim-tree")
-require("plugins/lualine")
-require("plugins/treesitter")
-require("plugins/diffview")
-require("plugins/blamer")
-require("plugins/indent-blankline")
-require("plugins/illuminate")
-require("plugins/null-ls")
-require("plugins/refactoring")
+require("plugins")
