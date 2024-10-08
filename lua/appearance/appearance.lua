0429d7b22bd565d998800eefbae3262a6f9e7b9a
Pianomyn <anditan@skiff.com>
2024-10-08 11:44:36 +1100

    highlight active tab in light blue

--- lua/appearance/appearance.lua
+++ lua/appearance/appearance.lua
@@ -2,10 +2,11 @@ local theme_mapping = require("appearance.theme_mapping")
 
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
--- vim.cmd("hi TabLineSel guibg=#223e55 guifg=#9CDCFE")
--- vim.cmd("hi TabLineSel guifg=LightGreen guibg=DarkGreen ctermfg=LightGreen ctermbg=DarkGreen ")
 
 vim.cmd.colorscheme(theme_mapping.current_color_scheme)
+
+vim.cmd("hi TabLineSel guibg=#223e55 guifg=#9CDCFE")
+-- vim.cmd("hi TabLineSel guifg=LightGreen guibg=DarkGreen ctermfg=LightGreen ctermbg=DarkGreen ")
 
 local M = {}
 M.lualine_theme = theme_mapping[theme_mapping.current_color_scheme]
