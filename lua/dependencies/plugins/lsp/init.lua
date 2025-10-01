2d29ac7443753d3db16c037bc8b626b20a1f74f0
Pianomyn <anditan2014@gmail.com>
2025-10-01 18:08:59 +1000

    removes unused empty file

--- lua/dependencies/plugins/lsp/init.lua
+++ lua/dependencies/plugins/lsp/init.lua
@@ -1,5 +1,4 @@
 require("dependencies.plugins.lsp.lspconfig")
 require("dependencies.plugins.lsp.lsp_signature")
 require("dependencies.plugins.lsp.toggle_lsp_diagnostics")
-require("dependencies.plugins.lsp.nvim_jdtls")
 
